2fd29e8f7166e66cdbae6b300bc74e4e866f6eee
Ignat Remizov <ignat980@gmail.com>
2022-04-22 14:21:40 +0300

    Fix speed shortcuts script to not trigger when typing in twitch chat

--- Set media playrate shortcuts.user.js
+++ Set media playrate shortcuts.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Set media playrate shortcuts
 // @namespace    http://ignat.space/
-// @version      6.0
+// @version      6.1
 // @author       Ignat Remizov
 // @description  Set every media element's playback rate. https://imgur.com/a/doFZXOI - visual explanation. Works for local mp4 files. Make sure "Run only in top frame:" setting is set to "No". Set to 1 by pressing 'y', 2 by douple-tapping 'y', 3 by tapping 'u', 3.5 by double-tapping 'y', 1.5 by tapping 'y' then 'u', and 2.5 by tapping 'u' then 'y'. Set to slower speeds by holding shift. You can also set media to loop by pressing 'p'.
+// @downloadURL  https://github.com/ignat980/Userscripts/raw/master/Set%20media%20playrate%20shortcuts.user.js
 // @updateURL    https://github.com/ignat980/Userscripts/raw/master/Set%20media%20playrate%20shortcuts.user.js
 // @match        *://*/*
 // @grant        none
@@ -37,7 +38,7 @@
     var prevTimeStamp = 0;
     window.addEventListener("keydown", function(e) {
         //If writing something in a text field (like a comment or a search), don't trigger a speed change
-        if (e.target.nodeName == "TEXTAREA" || e.target.nodeName == "INPUT" || e.target.nodeName == "YT-FORMATTED-STRING" || e.target.id == "contenteditable-root") return;
+        if (e.target.nodeName == "TEXTAREA" || e.target.nodeName == "INPUT" || e.target.nodeName == "YT-FORMATTED-STRING" || e.target.id == "contenteditable-root" || (e.target.getAttribute("role") === "textbox")) return;
         var key = e.keyCode ? e.keyCode : e.which;
         var duration = e.timeStamp - prevTimeStamp;
         var KEYPRESS_INTERVAL = 300;
