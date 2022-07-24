37af86f32b49e3e7d320cb009a789488adbcb740
Ignat Remizov <ignat980@gmail.com>
2022-07-24 15:03:37 +0300

    Fix speed script triggering on social media sites
    Instead of having a dozen different checks for node id's or custom data tags, almost all text inputs have the contenteditable attribute. Also, keyCode is deprecated, and just e.code will now be used. Much better!

--- Set media playrate shortcuts.user.js
+++ Set media playrate shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Set media playrate shortcuts
 // @namespace    http://ignat.space/
-// @version      6.1
+// @version      6.2
 // @author       Ignat Remizov
 // @description  Set every media element's playback rate. https://imgur.com/a/doFZXOI - visual explanation. Works for local mp4 files. Make sure "Run only in top frame:" setting is set to "No". Set to 1 by pressing 'y', 2 by douple-tapping 'y', 3 by tapping 'u', 3.5 by double-tapping 'y', 1.5 by tapping 'y' then 'u', and 2.5 by tapping 'u' then 'y'. Set to slower speeds by holding shift. You can also set media to loop by pressing 'p'.
 // @downloadURL  https://github.com/ignat980/Userscripts/raw/master/Set%20media%20playrate%20shortcuts.user.js
@@ -13,14 +13,17 @@
 (function() {
     'use strict';
     console.log("Running media playrate shortcuts script on", document); //Useful for debugging which iframe the script is running on
+
     //Config
-    var [KEY_SLOW, KEY_FAST, KEY_LOOP] = [89, 85, 80]; //keys 'Y', 'U', and 'P' on a standard QWERTY keyboard - change with codes from https://keycode.info/ if you want (careful on YouTube, there are some keys that are already bound, check by pressing "shift+/" - "?")
+    var [KEY_SLOW, KEY_FAST, KEY_LOOP] = ["KeyY", "KeyU", "KeyP"]; //careful changing to other keys on YouTube, there are many keys that are already bound and not changeable, check by pressing "shift+/" - "?"
+
     //Functions
     function setSpeed(speed) {
         for (let video of document.getElementsByTagName('video')) {
             //console.log("Setting speed to " + speed + " on", video);
             video.playbackRate = speed;
         }
+        //For podcasts
         for (let audio of document.getElementsByTagName('audio')) {
             //console.log("Setting speed to " + speed + " on", audio);
             audio.playbackRate = speed;
@@ -34,12 +37,19 @@
             audio.loop = state;
         }
     }
+
+    // Main
     var lastPressed;
     var prevTimeStamp = 0;
     window.addEventListener("keydown", function(e) {
         //If writing something in a text field (like a comment or a search), don't trigger a speed change
-        if (e.target.nodeName == "TEXTAREA" || e.target.nodeName == "INPUT" || e.target.nodeName == "YT-FORMATTED-STRING" || e.target.id == "contenteditable-root" || (e.target.getAttribute("role") === "textbox")) return;
-        var key = e.keyCode ? e.keyCode : e.which;
+        if (
+            e.target.nodeName === "TEXTAREA" || 
+            e.target.nodeName === "INPUT" ||
+            (e.target.getAttribute("contenteditable") === "true") ||
+            (e.target.getAttribute("role") === "textbox")
+        ) return;
+        var key = e.code;
         var duration = e.timeStamp - prevTimeStamp;
         var KEYPRESS_INTERVAL = 300;
         switch (key) {
